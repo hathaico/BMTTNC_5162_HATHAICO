dde9082fdf91ef76ff4b0facf2cb4e90f9f4791d
hathaico <hathaico3012@gmail.com>
2025-06-08 14:35:55 +0700

    [add] lab-01 ex01 ex02

---
+++ lab-01/ex01/hello.py
@@ -0,0 +1,3 @@
+print("hello,World!")
+print("My name is Co")
+print("Hutech University")
---
+++ lab-01/ex02/ex02_3.py
@@ -0,0 +1,6 @@
+so = int(input("nhập một số nguyên: "))
+ 
+if so % 2 == 0:
+    print(so,"là số chẳn.")
+else: 
+    print(so,"không phải là số chẳn.")
