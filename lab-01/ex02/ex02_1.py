84366989e5c9904d68d8ff24f790b8548f2feadd
hathaico <hathaico3012@gmail.com>
2025-06-08 20:38:15 +0700

    [add] lab-04

--- lab-01/ex02/ex02_1.py
+++ lab-01/ex02/ex02_1.py
@@ -1,5 +1,5 @@
 # Nhập tên và tuổi từ người dùng
-ten = input("nhập tên của bạn")
-tuoi = input("nhập tuổi của bạn")
+ten = input("nhập tên của bạn: ")
+tuoi = input("nhập tuổi của bạn: ")
 # In thông điệp chào mừng với thông tin vừa nhập
 print(" chào mừng,",ten, "! Bạn",tuoi, "tuổi.")
